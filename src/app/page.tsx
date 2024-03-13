334f6fd838b65cf8dc72cb53021950203e0b7311
Ivan Ushakov <ivan.ushakov@raiffeisen.ru>
2024-03-13 18:32:42 +0300

    Added conect with vercel db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import { sql } from '@vercel/postgres';
 // import styles from './page.module.css';
 
-export default function Home() {
+export default async function Home() {
+    const { rows } = await sql`SELECT * FROM users`;
+
     return (
         <main>
             <p>Main page</p>
+            {rows.length > 0 &&
+                rows.map((user) => (
+                    <p key={user.id}>{`${user.name} by id: ${user.id}`}</p>
+                ))}
         </main>
     );
 }

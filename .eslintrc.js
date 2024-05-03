7b67a4d95bff2981873a6973f9539c2bc722bbb9
Ivan Ushakov <ivan.ushakov@raiffeisen.ru>
2024-05-03 13:22:06 +0300

    feat: Change eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,17 @@
 module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    extends: ['eslint:recommended', 'next/core-web-vitals', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'next/core-web-vitals'],
+    rules: {
+        '@typescript-eslint/ban-ts-ignore': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+    },
 };
